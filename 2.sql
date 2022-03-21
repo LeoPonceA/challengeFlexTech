2262dec69766985ee1024d1609b8a7c287229447
LeoPonceA <102045217+LeoPonceA@users.noreply.github.com>
2022-03-21 12:36:34 -0300

    Add files via upload
    2. Seleccione todas las divisiones que han tenido ingresos este año

---
+++ 2.sql
@@ -0,0 +1,17 @@
+-- TABLE
+CREATE TABLE convers(
+ID int not null,
+valor int, 
+primary key(ID));
+CREATE TABLE conversion(
+ID int not null,
+valor int, 
+primary key(ID));
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
