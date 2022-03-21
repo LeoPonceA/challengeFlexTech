9853f55c4eb0fd66b18ff57ee76d500eba064a6c
LeoPonceA <102045217+LeoPonceA@users.noreply.github.com>
2022-03-21 14:37:24 -0300

    Add files via upload
    10. Suma las edades

---
+++ 10.sql
@@ -0,0 +1,20 @@
+--Creamos la tabla con sus campos
+create table edad(
+ID int not null,
+name varchar(6),
+age int, 
+primary key(ID));
+
+insert into edad (id, name, age) values(1, "bob", 21);
+insert into edad (id, name, age) values(2, "Sam", 19);
+insert into edad (id, name, age) values(3, "Jill", 18);
+insert into edad (id, name, age) values(4, "Jim", 21);
+insert into edad (id, name, age) values(5, "Sally", 19);
+insert into edad (id, name, age) values(6, "Jess", 20);
+insert into edad (id, name, age) values(7, "Will", 21);
+
+--Sumamos las edades
+select sum(age) as 'Sumatoria'
+from edad;
+
+DROP table edad;
