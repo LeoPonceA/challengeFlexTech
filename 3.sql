b3afd6124d5b431c84a31d6f54ca35dce1de2f84
LeoPonceA <102045217+LeoPonceA@users.noreply.github.com>
2022-03-21 13:04:29 -0300

    Add files via upload
    3. Encuentra el siglo para el año

---
+++ 3.sql
@@ -0,0 +1,21 @@
+-- TABLE
+CREATE TABLE anios(
+ID int not null ,
+year int,
+primary key(ID));
+CREATE TABLE convers(
+ID int not null,
+valor int, 
+primary key(ID));
+CREATE TABLE conversion(
+ID int not null,
+valor int, 
+primary key(ID));
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
