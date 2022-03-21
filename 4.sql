7e7b64d03481f9a77543f75ad62d33cc78c1c796
LeoPonceA <102045217+LeoPonceA@users.noreply.github.com>
2022-03-21 13:33:19 -0300

    Add files via upload
    4. Par o impar

---
+++ 4.sql
@@ -0,0 +1,26 @@
+-- TABLE
+CREATE TABLE anios(
+ID int not null ,
+year int,
+primary key(ID));
+CREATE TABLE convers(
+ID int not null,
+valor int, 
+primary key(ID));
+CREATE TABLE conversion(
+ID int not null,
+valor int, 
+primary key(ID));
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE edad(
+ID int not null,
+name varchar(6),
+age int, 
+primary key(ID));
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
